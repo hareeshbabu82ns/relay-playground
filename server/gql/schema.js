12af3348fd294f575011c27de591ed984cd867df
Hareesh <hareeshbabu82ns@gmail.com>
2017-07-13 19:30:29 -0600

    graphql with sample mongodb models

--- server/gql/schema.js
+++ server/gql/schema.js
@@ -10,7 +10,23 @@ import mongoose from "mongoose";
 
 const Link = mongoose.model("Link");
 
+const LinkType = new GraphQLObjectType({
+  name: "Link",
+  fields: () => ({
+    title: { type: GraphQLString },
+    url: { type: GraphQLString }
+  })
+});
+const CounterType = new GraphQLObjectType({
+  name: "Counter",
+  fields: () => ({
+    counter: {
+      type: GraphQLInt
+    }
+  })
+});
 let counter = 100;
+const data = [{ counter: 33 }, { counter: 88 }, { counter: 43 }];
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -19,6 +35,14 @@ const schema = new GraphQLSchema({
       counter: {
         type: GraphQLInt,
         resolve: () => counter
+      },
+      data: {
+        type: new GraphQLList(CounterType),
+        resolve: () => data
+      },
+      links: {
+        type: new GraphQLList(LinkType),
+        resolve: () => Link.find({})
       }
     })
   }),
